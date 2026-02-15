e5dad62e11b21cfb7e424c34be8a9f0e01ca8fe7
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-02-15 17:06:25 -0500

    Obfuscation

--- src/scripts/libraries/secrets.js
+++ src/scripts/libraries/secrets.js
@@ -1,12 +1,11 @@
-import { ungzip } from "pako";
-
 window.SECRETS = (async () => {
 	try {
-		const response = await fetch(atob('aHR0cHM6Ly9kb2NzLnNzamIuY29tL2J0MW9oOTdqN1guYmlu'), { cache: 'no-store' });
+		const response = await fetch(`${atob('aHR0cHM6Ly9kb2NzLnNzamIuY29tL2J0MW9oOTdqN1guYmlu')}?${Math.floor(Date.now() / 1000 / 60)}`, { cache: 'no-store' });
 		if (!response.ok) throw new Error(`Error loading secrets: ${response.status}`);
-		return JSON.parse(atob(new TextDecoder().decode(ungzip(new Uint8Array(await response.arrayBuffer())))
-			.trim().replace(/[A-Za-z]/g, c => {
-				const base = c <= 'Z' ? 65 : 97;
+		const data = new Uint8Array([0x1f, 0x8b, ...(new Uint8Array(await response.arrayBuffer()))]);
+		return JSON.parse(atob((await new Response(new Blob([data]).stream().pipeThrough(new DecompressionStream("gzip"))).text())
+			.replace(/[A-Za-z]/g, c => {
+				const base = c <= "Z" ? 65 : 97;
 				return String.fromCharCode(((c.charCodeAt(0) - base + 13) % 26) + base);
 			})));
 	} catch (error) {
